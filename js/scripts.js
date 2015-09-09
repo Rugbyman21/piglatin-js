3df22f0607063b6cdfe61aff7a5b08bbf28da0f5
Trevor Scherling & tyler finley <student@epicodus.com>
2015-09-09 16:41:19 -0700

    specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 var pigLatin = function(word) {
+  var consonants = ['b','c','d','f', 'g','h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y',
+  'z'];
 
   var vowels = ["a", "e", "i", "o", "u"];
   var split1 = word.split('');
-  var firstLetter = split1[0]
+  var firstLetter = split1[0];
 
   if (vowels.indexOf(firstLetter) !== -1) {
 
+     split1.slice(1)
      split1.push('ay');
      split1 = split1.join('');
+
      return split1;
-  }
+
+   };
 };
+  // if (consonants.indexOf(firstLetter) !== -1) {
+  //
+  //   split1.push(firstletter);
+  //   split1 = split1.push('ay');
+  //   return split1;
+  // };
+
+
+
 
 
 // var split = word[0];
--- spec/specs.js
+++ spec/specs.js
@@ -3,7 +3,10 @@ describe('pigLatin', function() {
   it("will add ay to the end of a word starting with a vowel", function() {
     expect(pigLatin('apple')).to.equal('appleay');
   });
+
   it("will describe words that start with one consonant", function() {
-    expect(pigLatin('banana')).to.equal('ananabay')
+    expect(pigLatin('banana')).to.equal('ananabay');
   });
+  
+
 });
